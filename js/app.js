5697a9ab496e799575b4b0dabea6c77664a4a40b
Aurelia Moser <auremoser@gmail.com>
2014-12-08 18:16:51 -0300

    yay query gui works

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 'use strict'
 /* Take textarea input and make request to API */
 
-var query = $('#query-textarea').val();
-query =  sanitizeQuery(query);
 // format string from textfield to send request
 // remove carriage returns etcet
 
@@ -12,19 +10,36 @@ function sanitizeQuery(query){
    return query;
 }
 
-// make ajax request based on query
+// submit query request and render result
+
 function makeRequest(query){
+	$('#submit').attr('disable', true).css({opacity:0.2});
+	$('#result').val('Sending request...');
+	console.log(query);
 	$.ajax({
-	    type: 'GET',
-	    url: 'http://osm.cartodb.com/api/v2/sql',
-	    data: {
-	        format: 'geojson',
-	        q:query
-	    },
-	    dataType: "json"
-	}).done(function(data) {
-	    console.log(data)
-	}).fail(function() {
-	    alert("error");
+      type: 'GET',
+      url: 'http://osm.cartodb.com/api/v2/sql',
+      data: {
+        format: 'geojson',
+        q:query
+      },
+      dataType: "json"
+    }).done(function(data) {
+        $('#result').val(JSON.stringify(data));
+    }).fail(function(jqXhr, status, error) {
+        $('#result').val("error"+status+error);
+		console.error('ERROR', status, error);
+    }).always(function(){
+    	$('#submit').attr('disable', null).css({opacity:1});
 	});
 }
+
+function handleSubmit(){
+	var query = $('#query').val();
+	query = sanitizeQuery(query);
+	makeRequest(query);
+}
+
+$(function(){
+	$('#submit').on('click', handleSubmit);
+});
