4db6f4cae9ed69f04306e4bb84c6837a4e312c0c
Aurelia Moser <auremoser@gmail.com>
2014-12-08 13:51:31 -0300

    tried new things, not working

--- js/app.js
+++ js/app.js
@@ -1,25 +1,30 @@
 'use strict'
-/* GUI SCRIPT*/
-/* Take GUI text and make request to API */
+/* Take textarea input and make request to API */
 
+var query = $('#query-textarea').val();
+query =  sanitizeQuery(query);
 // format string from textfield to send request
 // remove carriage returns etcet
-function formatQuery(){
-	var query = "SELECT ST_Centroid(the_geo m) as the_geom, \n tags->’name’ as name \n FROM planet \n WHERE tags@>'aeroway=>aerodrome' \n AND the_geom && ST_MakeEnvelope(-125.0, 24.9, -66.9, 49.5,4326)";
-	query = query.replace(/(\r\n|\n|\r)/gm, '');
+
+function sanitizeQuery(query){
+   query = query.replace(/’/gm, "'"); //remove the weird quote symbol, replace with '
+   query = query.replace(/(\r\n|\n|\r)/gm, '');//remove all new lines
+   return query;
 }
 
 // make ajax request based on query
-$.ajax({
-    type: 'GET',
-    url: 'http://osm.cartodb.com/api/v2/sql',
-    data: {
-        format: 'geojson',
-        q: "SELECT ST_Centroid(the_geom) as the_geom, tags->'name' as name FROM planet WHERE tags@>'aeroway=>aerodrome' AND the_geom && ST_MakeEnvelope(-125.0, 24.9, -66.9, 49.5,4326)"
-    },
-    dataType: "json"
-}).done(function(data) {
-    console.log(data)
-}).fail(function() {
-    alert("error");
-});
+function makeRequest(query){
+	$.ajax({
+	    type: 'GET',
+	    url: 'http://osm.cartodb.com/api/v2/sql',
+	    data: {
+	        format: 'geojson',
+	        q:query
+	    },
+	    dataType: "json"
+	}).done(function(data) {
+	    console.log(data)
+	}).fail(function() {
+	    alert("error");
+	});
+}
